d76d84c8612dcadf503857a7e2886cffa76ef55d
dinhtrung <nguyendinhtrung141@gmail.com>
2019-01-16 11:03:41 -0500

    Initial commit.

---
+++ app/shared/reducers/create-store.js
@@ -0,0 +1,46 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import RehydrationServices from '../services/rehydration.service'
+import ReduxPersist from '../../config/redux-persist'
+import Config from '../../config/debug-config'
+import WebsocketService from '../websockets/websocket-service'
+// creates the store
+export default (rootReducer, rootSaga) => {
+  /* ------------- Redux Configuration ------------- */
+
+  const middleware = []
+  const enhancers = []
+
+  /* ------------- Saga Middleware ------------- */
+
+  const sagaMonitor = Config.useReactotron ? console.tron.createSagaMonitor() : null
+  const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
+  middleware.push(sagaMiddleware)
+  const wsSagaMiddleware = createSagaMiddleware(WebsocketService.websocketSagas)
+  middleware.push(wsSagaMiddleware)
+
+  /* ------------- Assemble Middleware ------------- */
+
+  enhancers.push(applyMiddleware(...middleware))
+
+  // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
+  const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore
+  const store = createAppropriateStore(rootReducer, compose(...enhancers))
+
+  // configure persistStore and check reducer version number
+  if (ReduxPersist.active) {
+    RehydrationServices.updateReducers(store)
+  }
+
+  // kick off root saga
+  let sagasManager = sagaMiddleware.run(rootSaga)
+  let websocketSagaManager = wsSagaMiddleware.run(WebsocketService.websocketSagas)
+
+  return {
+    store,
+    sagasManager,
+    websocketSagaManager,
+    sagaMiddleware
+  }
+}
